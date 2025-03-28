668286d395e73a0a48ce1887b5cea97c6e57ac7a
Your Name <you@example.com>
2025-03-27 23:39:50 -0300

    Altereção de dados demos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,8 +4,8 @@ describe('Login', () => {
 
     cy.visit('https://playground.cyskills.com.br')
 
-    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
-    cy.get('[data-cy="password"]').type('showtime')
+    cy.get('[data-cy="email"]').type('email@email.com')
+    cy.get('[data-cy="password"]').type('senhausada123')
     cy.get('[data-cy="login-button"]').click()
 
     cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
